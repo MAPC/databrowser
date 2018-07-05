85169f41362b8c3961d0499da3b90fd25959198b
Eric Youngberg <eric@lmtlss.net>
2018-07-05 13:43:28 -0400

    Restructure and populate header

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,18 +1,24 @@
-import Ember from 'ember';
+import Controller from '@ember/controller';
+import { computed } from '@ember-decorators/object';
+import config from 'databrowser/config/environment';
 
-export default Ember.Controller.extend({
-  queryParams: ['selectedCategory'],
-  selectedCategory: null,
-  primaryCount: 0,
-  searchables: Ember.computed('model', function() {
-    let model = this.get('model');
-    let structured = [];
 
-    model.forEach(function(dataset) {
-      structured.push({ title: dataset.get('menu3'), 
-                        id: dataset.get('id')       });
-    });
+export default class extends Controller {
 
-    return structured;
-  })
-});
+  constructor() {
+    super(...arguments);
+
+    this.queryParams = ['selectedCategory'];
+    this.selectedCategory = null;
+    this.primaryCount = 0;
+    this.hostname = config.metadataHost;
+  }
+
+
+  @computed('model')
+  get searchables() {
+    return this.get('model')
+               .map(row => ({ id: row.get('id'), title: row.get('menu3') }));
+  }
+
+}
