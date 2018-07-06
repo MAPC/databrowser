5ac0aa531daecf4aaff6f77ebb15d9b5600761c1
Eric Youngberg <eric@lmtlss.net>
2018-07-06 13:26:30 -0400

    Implement new category selection lists

--- app/components/menus-component.js
+++ app/components/menus-component.js
@@ -1,23 +1,34 @@
-import Ember from 'ember';
+import Component from '@ember/component';
 import { nest } from 'd3-collection';
+import { computed, action } from '@ember-decorators/object';
 
-export default Ember.Component.extend({
-  classNames: ['ui','selection','list'],
-  groupColumn: 'menu1',
-  subRoute: 'categories.sub-categories',
-  model: null,
-  primary: Ember.computed('model', function() {
-    let model = this.get('model');
-    let groupColumn = this.get('groupColumn');
-    let grouped = nest()
-      .key((d) => d.get(groupColumn))
-      .entries(model.toArray());
-    return grouped;
-  }),
-  actions: {
-    routeTo(category) {
-      let subRoute = this.get('subRoute');
-      this.get('router').transitionTo(subRoute, category);
-    }
+
+export default class extends Component {
+
+  constructor(params) {
+    super(...arguments);
+
+    this.classNames = ['component', 'menus-component'];
+    this.subRoute = params.attrs.subRoute || 'categories.sub-categories';
+    this.groupColumn = params.attrs.groupColumn || 'menu1';
   }
-});
+
+
+  @computed('model')
+  get primary() {
+    const model = this.get('model');
+    const groupColumn = this.get('groupColumn');
+
+    return nest().key(d => d.get(groupColumn))
+                 .entries(model.toArray());
+  }
+
+
+  @action
+  routeTo(category) {
+    const subRoute = this.get('subRoute')
+
+    this.get('router').transitionTo(subRoute, category);
+  }
+
+}
